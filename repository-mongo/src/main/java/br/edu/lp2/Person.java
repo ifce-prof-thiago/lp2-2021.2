68a9bcbb64ceb42de812ae6e1d0aa26f52125f85
thiago1fc3 <thiago@lar.ifce.edu.br>
2022-05-03 14:02:10 -0300

    chore(Infrastructure): criação do repositório mongo

---
+++ repository-mongo/src/main/java/br/edu/lp2/Person.java
@@ -0,0 +1,14 @@
+package br.edu.lp2;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Person {
+
+    private String id;
+    private String name;
+    private int age;
+
+}
